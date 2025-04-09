d0dee521abffbea7b26a8e3892e7b3b9f4aeea8a
Pablo Bandeira <63755365+acerbityroque@users.noreply.github.com>
2025-04-09 15:39:48 -0300

    Configure cypress cloud

--- cypress/e2e/bugbank.cy.ts
+++ cypress/e2e/bugbank.cy.ts
@@ -1,28 +1,38 @@
-describe('Fluxo de Cadastro e Login | BugBank', () => {
-  const credenciais = {
-      email: 'pablo@gmail.com',
-      senha: '123456'
-  };
+describe('Fluxo completo de Cadastro, Login e Logout | BugBank', () => {
+    const credenciais = {
+        email: 'pablo@gmail.com',
+        senha: '123456',
+        nome: 'Pablo'
+    };
 
-  before(() => {
-      // Realiza o cadastro (CT03)
-      cy.visit('https://bugbank.netlify.app/');
-      cy.xpath("//button[text()= 'Registrar']").click();
-      cy.xpath("(//label[text()='E-mail']//following-sibling::input)[2]").type(credenciais.email, { force: true, delay: 100 });
-      cy.xpath("/html/body/div/div/div[2]/div/div[2]/form/div[3]/input").type('Pablo', { force: true, delay: 100 });
-      cy.xpath("/html/body/div/div/div[2]/div/div[2]/form/div[4]/div/input").type(credenciais.senha, { force: true, delay: 100 });
-      cy.xpath("/html/body/div[1]/div/div[2]/div/div[2]/form/div[5]/div/input").type(credenciais.senha, { force: true, delay: 100 });
-      cy.xpath("/html/body/div[1]/div/div[2]/div/div[2]/form/div[6]/label/span").click({ force: true });
-      cy.get('.styles__ContainerFormRegister-sc-7fhc7g-0 > .style__ContainerButton-sc-1wsixal-0').click({ force: true });
-      cy.get('#btnCloseModal').should('be.visible').click({ force: true });
-  });
+    beforeEach(() => {
+        // CT03: Cadastro com dados válidos
+        cy.visit('https://bugbank.netlify.app/');
+        cy.xpath("//button[text()= 'Registrar']").click();
+        cy.xpath("(//label[text()='E-mail']//following-sibling::input)[2]").type(credenciais.email, { force: true, delay: 100 });
+        cy.xpath("/html/body/div/div/div[2]/div/div[2]/form/div[3]/input").type(credenciais.nome, { force: true, delay: 100 });
+        cy.xpath("/html/body/div/div/div[2]/div/div[2]/form/div[4]/div/input").type(credenciais.senha, { force: true, delay: 100 });
+        cy.xpath("/html/body/div[1]/div/div[2]/div/div[2]/form/div[5]/div/input").type(credenciais.senha, { force: true, delay: 100 });
+        cy.xpath("/html/body/div[1]/div/div[2]/div/div[2]/form/div[6]/label/span").click({ force: true });
+        cy.get('.styles__ContainerFormRegister-sc-7fhc7g-0 > .style__ContainerButton-sc-1wsixal-0').click({ force: true });
+         // Adiciona um delay de 6 segundos antes do login
+         cy.wait(6000);
+        cy.get('#btnCloseModal').should('be.visible').click({ force: true });
+        
+    });
 
-  it('Realiza login com as credenciais cadastradas (CT01)', () => {
-      // Faz login com as credenciais criadas no CT03
-      cy.visit('https://bugbank.netlify.app/');
-      cy.xpath("(//label[text()='E-mail']//following-sibling::input)[1]").type(credenciais.email, { force: true, delay: 100 });
-      cy.xpath("(//label[text()='Senha']//following-sibling::input)[1]").type(credenciais.senha, { force: true, delay: 100 });
-      cy.xpath("//button[text()='Acessar']").click();
 
-  });
+    it.only('CT 01 e 03, Login e Logout', () => {
+        // CT09: Login e Logout
+        cy.xpath("(//label[text()='E-mail']//following-sibling::input)[1]").type(credenciais.email, { force: true, delay: 100 });;
+        cy.xpath('/html/body/div/div/div[2]/div/div[1]/form/div[2]/div/input').type(credenciais.senha, { force: true, delay: 100 });
+        cy.xpath("/html/body/div/div/div[2]/div/div[1]/form/div[3]/button[1]").click(); // Botão "Entrar"
+         // Adiciona um delay de 6 segundos antes do login
+         cy.wait(6000);
+        cy.xpath("/html/body/div/div/div[1]/div/a").click(); // Botão "Sair" na tela logada
+
+        // Validações pós-logout
+        cy.url().should('include', ''); // Confirma que retornou à tela de login
+        cy.xpath("(//label[text()='E-mail']//following-sibling::input)[1]").should('be.visible'); // Verifica que o campo de e-mail está visível
+    });
 });
